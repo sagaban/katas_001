865f7c004a194a063e447f9bc295811e216e814a
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2015-06-12 19:22:44 -0300

    Done point 1

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,5 +1,13 @@
 var calculator = {
     add: function(numbers){
+        if (numbers){
+            var array =  numbers.split(',');
+            var sum = 0;
+            for (var i in array){
+                sum = sum + Number(array[i]);
+            }
+            return sum;
+        }
         return 0;
     }
 }
--- test/spec/test.js
+++ test/spec/test.js
@@ -7,6 +7,12 @@
             it('should return 0 for the empty string', function () {
                 expect(calculator.add("")).toEqual(0);
             });
+            it('should return the number past as parameter', function () {
+                expect(calculator.add("3")).toEqual(3);
+            });
+            it('should return the sum of the numbers past as parameter', function () {
+                expect(calculator.add("3,2")).toEqual(5);
+            });
         });
     });
 })();
