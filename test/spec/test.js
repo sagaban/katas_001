2848771ae9f37d2698015daf0c8e768a1fb86520
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2015-06-14 17:59:29 -0300

    Done point 5

--- test/spec/test.js
+++ test/spec/test.js
@@ -13,24 +13,25 @@
             });
             it('should return the sum of the numbers past as parameter', function () {
                 expect(calculator.add("3,2")).toEqual(5);
-                expect(calculator.add("-2,2")).toEqual(0);
-                expect(calculator.add("-5,-1")).toEqual(-6);
+                expect(function(){calculator.add("-2,2")}).toThrow();
+                expect(function(){calculator.add("-5,-1")}).toThrow();
             });
 
             it('should return the sum of ANY numbers past as parameter', function () {
-                expect(calculator.add("-5,-1,12")).toEqual(6);
+                expect(function(){calculator.add("-5,-1,12")}).toThrow();
                 expect(calculator.add("1,2,3")).toEqual(6);
                 expect(calculator.add("0,2,3")).toEqual(5);
             });
             it('should be able to use "," or \\n', function () {
-                expect(calculator.add("-5\n-1,12")).toEqual(6);
+                expect(function(){calculator.add("-5\n-1,12")}).toThrow();
                 expect(calculator.add("1,2\n3")).toEqual(6);
                 expect(calculator.add("0\n2\n3")).toEqual(5);
             });
 
             it('The string calculator should support different delimiters', function () {
                 expect(calculator.add("//;\n1;2")).toEqual(3);
-
+                expect(calculator.add("//*\n4*5*5")).toEqual(14);
+                expect(calculator.add("//$\n1$2$7")).toEqual(10);
             });
         });
     });
