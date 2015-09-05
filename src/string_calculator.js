6eaf7375c3dec5eb3959f501edb2b0bf7b136263
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2015-09-05 15:18:32 +0200

    Change indentation. Format readme

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,31 +1,31 @@
 var calculator = {
-    add: function(numbers){
-        var sum = 0,
-            re,
-            negatives = [];
-        if (numbers){
-            if (numbers.substr(0,2) === '//'){
-                re = new RegExp(quote(numbers.substr(2,1)));
-                numbers = numbers.substr(4);
-            } else{
-                re = /[,\n]/;
-            }
-            var array =  numbers.split(re);
-            array.forEach(function(element, index, array){
-                if (Number(element) < 0){
-                    negatives.push(Number(element))
-                }
-                sum = sum + Number(element);
-            })
+  add: function (numbers) {
+    var sum = 0,
+      re,
+      negatives = [];
+    if (numbers) {
+      if (numbers.substr(0, 2) === '//') {
+        re = new RegExp(quote(numbers.substr(2, 1)));
+        numbers = numbers.substr(4);
+      } else {
+        re = /[,\n]/;
+      }
+      var array = numbers.split(re);
+      array.forEach(function (element, index, array) {
+        if (Number(element) < 0) {
+          negatives.push(Number(element))
         }
-        if (negatives.length > 0){
-            throw Error("negatives not allowed: " + negatives);
-        } else {
-            return sum;
-        }
+        sum = sum + Number(element);
+      })
     }
+    if (negatives.length > 0) {
+      throw Error("negatives not allowed: " + negatives);
+    } else {
+      return sum;
+    }
+  }
 }
 
-var quote = function(str) {
-    return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+var quote = function (str) {
+  return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
 };
